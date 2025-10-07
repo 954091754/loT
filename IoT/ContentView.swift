e8bdcc276f5f501e86618cef9c8b12ef66f75d3d
Jesion <954091754@qq.com>
2025-10-07 12:53:24 +0800

    init loT project

---
+++ IoT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IoT
+//
+//  Created by 罗兴志 on 2025/10/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IoT/IoTApp.swift
@@ -0,0 +1,17 @@
+//
+//  IoTApp.swift
+//  IoT
+//
+//  Created by 罗兴志 on 2025/10/7.
+//
+
+import SwiftUI
+
+@main
+struct IoTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
